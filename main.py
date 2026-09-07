4745a5da9acdcd231d656f8a686f45e9370e3c87
Manan79 <manansood732004@gmail.com>
2026-09-07 20:05:51 +0530

    Minor Changes integrating LangSmith for Observabilty and Tracing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from travel-agent-ai!")
+    print("Hello from Manan")
 
 
 if __name__ == "__main__":
--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import date, timedelta
-
-
-# Add one day to get tomorrow
-tomorrow = date.today() + timedelta(days=1)
-
-print(tomorrow)
